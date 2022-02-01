308069bfe3b063277d6b58f201693d64adff878f
Nav Srijan <nav.srijan.official@gmail.com>
2022-02-01 13:45:31 +0530

    Can take backup as zip

--- __main__.py
+++ __main__.py
@@ -1,15 +1,17 @@
 #TheWhistler
 
 from parser import Parser
-from mover import copyFile
+from mover import *
 import pdb
 
 dst = "/home/thewhistler/Projects/playground/"
 
 par = Parser("files.nav")
-pdb.set_trace()
-for i in par.content:
-    for k in i[1]:
-        t = i[0].split("/")
-        fdst = dst + t[-2] + "/" + k
-        copyFile(i[0] + k, fdst)
+
+takeBackup("t1", par.content)
+
+#for i in par.content:
+#    for k in i[1]:
+#        t = i[0].split("/")
+#        fdst = dst + t[-2] + "/" + k
+#        copyFile(i[0] + k, fdst)
--- mover.py
+++ mover.py
@@ -1,6 +1,36 @@
 import shutil
 import os
+import pdb
+from zipfile import ZipFile
 
 def copyFile(filename, pathToCopyTo):
     os.makedirs(os.path.dirname(pathToCopyTo), exist_ok=True)
     shutil.copy(filename, pathToCopyTo)
+def takeBackup(backupName, pathList):
+    try:
+        os.mkdir("temp")
+    except:
+        pass
+    backupName = "temp/"+backupName
+    os.mkdir(backupName)
+    dst = backupName + "/"
+    for i in pathList:
+        for k in i[1]:
+            t = i[0].split("/")
+            fdst = dst + t[-2] + "/" + k
+            copyFile(i[0] + k, fdst)
+    createZIP(backupName, backupName)
+def getFilePaths(dirname):
+    paths = []
+    for root, directories, files in os.walk(dirname):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            paths.append(filepath)
+    return paths
+def createZIP(filename, directoryToZip):
+    pdb.set_trace()
+    filePaths = getFilePaths(directoryToZip)
+    with ZipFile(filename+".zip", 'w') as zip:
+        for file in filePaths:
+            zip.write(file)
+    shutil.rmtree(directoryToZip)
